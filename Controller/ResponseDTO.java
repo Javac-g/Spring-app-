44dc428c58ec69cf08744e94498876b3944fa1ba
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-22 17:14:32 +0300

    Response

--- Controller/ResponseDTO.java
+++ Controller/ResponseDTO.java
@@ -1,4 +1,46 @@
 package com.Maks.Controller;
 
+import com.Maks.Service.Pet_Enum;
+
 public class ResponseDTO {
+
+    String name;
+
+    Integer id;
+
+    Tool skill;
+
+    Pet_Enum pet;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Tool getSkill() {
+        return skill;
+    }
+
+    public void setSkill(Tool skill) {
+        this.skill = skill;
+    }
+
+    public Pet_Enum getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet_Enum pet) {
+        this.pet = pet;
+    }
 }
