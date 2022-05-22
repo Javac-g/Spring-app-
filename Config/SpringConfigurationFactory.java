42d1f228f94f08edad644a1377cd35b22ea1eee9
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-22 19:40:18 +0300

    вщdone

--- Config/SpringConfigurationFactory.java
+++ Config/SpringConfigurationFactory.java
@@ -1,4 +1,27 @@
 package com.Maks.Config;
 
+import com.Maks.Controller.ControlleR;
+import com.Maks.Service.Services;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class SpringConfigurationFactory {
+
+    @Bean
+    public Services servicesBean(){
+
+        return new Services();
+
+    }
+    @Bean
+    public ControlleR controllerBean(){
+
+        return new ControlleR(servicesBean());
+
+    }
+
+
+
 }
--- Controller/ControlleR.java
+++ Controller/ControlleR.java
@@ -1,4 +1,50 @@
 package com.Maks.Controller;
 
+import com.Maks.Service.RequestDTO;
+import com.Maks.Service.Services;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/VK")
 public class ControlleR {
+
+    public ControlleR(Services services) {
+
+        this.services = services;
+
+    }
+
+    private Services services;
+
+    @PostMapping(value = "/a",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO add(@RequestBody RequestDTO requestDTO){
+
+         return services.Create(requestDTO);
+
+    }
+    @GetMapping(value = "/f",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO find(@RequestParam Integer id){
+
+        return services.Read(id);
+
+    }
+
+    @PutMapping(value = "/u",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO update(@RequestParam Integer id,@RequestBody RequestDTO requestDTO){
+
+        return services.Update(id, requestDTO);
+
+    }
+
+    @DeleteMapping(value = "/d",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String delete(@RequestParam Integer id){
+
+        return services.print("Was deleted person № - ") + services.delete(id);
+
+    }
+
 }
