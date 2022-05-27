d1cf92d2118c8f97aefc391e16ef6fa92de6fa72
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-27 23:17:43 +0300

    find

--- V2/Controller/ControlleR.java
+++ V2/Controller/ControlleR.java
@@ -1,6 +1,9 @@
 package com.Maks.V2.Controller;
 
+import com.Maks.V2.Services.RequestDTO;
 import com.Maks.V2.Services.Services;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 public class ControlleR {
 
@@ -9,6 +12,28 @@ public class ControlleR {
     public ControlleR(Services services){
 
         this.services = services;
+
+    }
+
+    @PostMapping(value = "/a",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO add(@RequestParam RequestDTO requestDTO){
+
+        return services.create(requestDTO);
+
+    }
+
+    @GetMapping(value = "/f",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO find(@RequestParam Integer id){
+
+        return services.read(id);
+
+    }
+
+    public ResponseDTO update(@RequestParam Integer id, @RequestBody RequestDTO requestDTO){
+
+        return services.update(id,requestDTO);
 
     }
 }
