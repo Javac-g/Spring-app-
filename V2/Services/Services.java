21d09a3fb3da67fd48098f8e651a788f92eec09b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-27 21:01:05 +0300

    List

--- V2/Services/Services.java
+++ V2/Services/Services.java
@@ -1,5 +1,14 @@
 package com.Maks.V2.Services;
 
+import com.Maks.V2.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Services {
+
+
+    List<ResponseDTO> database = new ArrayList<>();
+
 
 }
