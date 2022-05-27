dd92047ebc2ee5f8e84f6c4706c9779b48b1b987
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-27 22:28:38 +0300

    Configuration factory

--- V2/Config/SpringConfigurationFactory.java
+++ V2/Config/SpringConfigurationFactory.java
@@ -1,4 +1,25 @@
 package com.Maks.V2.Config;
 
+
+import com.Maks.V2.Controller.ControlleR;
+import com.Maks.V2.Services.Services;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringConfigurationFactory {
+    @Bean
+    Services servicesBEAN(){
+
+        return  new Services();
+
+    }
+    @Bean
+    public ControlleR controlleRBEAN(){
+
+        return new ControlleR(servicesBEAN());
+    }
+
+
+
 }
--- V2/Controller/ControlleR.java
+++ V2/Controller/ControlleR.java
@@ -1,4 +1,14 @@
 package com.Maks.V2.Controller;
 
+import com.Maks.V2.Services.Services;
+
 public class ControlleR {
+
+    private Services services;
+
+    public ControlleR(Services services){
+
+        this.services = services;
+
+    }
 }
