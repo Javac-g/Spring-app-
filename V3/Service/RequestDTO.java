819d8199b1014361d77c68418b834c4f58be9c43
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-29 16:27:40 +0300

    2

---
+++ V3/Service/RequestDTO.java
@@ -0,0 +1,22 @@
+package com.Maks.V3.Service;
+
+public class RequestDTO {
+    String name;
+    Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
