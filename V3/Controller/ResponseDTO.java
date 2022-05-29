9f36e518548ddfb46b42c0fb0d00d5372f8dd6a0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-29 16:32:31 +0300

    3

---
+++ V3/Controller/ResponseDTO.java
@@ -0,0 +1,23 @@
+package com.Maks.V3.Controller;
+
+public class ResponseDTO {
+
+    String name;
+    Integer id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
