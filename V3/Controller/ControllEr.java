b922abb7a33be8a1a17ff5f2f9bf6ad6ac3d70b2
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-29 16:45:40 +0300

    9

--- V3/Controller/ControllEr.java
+++ V3/Controller/ControllEr.java
@@ -1,4 +1,14 @@
 package com.Maks.V3.Controller;
 
+import com.Maks.V3.Service.Services;
+
 public class ControllEr {
+
+
+    private Services services;
+
+    public ControllEr(Services services) {
+
+        this.services = services;
+    }
 }
