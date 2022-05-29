c13cdf544ef28da871dbc05e0abf8ca9635160b4
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-29 23:42:39 +0300

    1

--- V3/Configuration/SpringConfigurationFactory.java
+++ V3/Configuration/SpringConfigurationFactory.java
@@ -4,17 +4,20 @@ import com.Maks.V3.Controller.ControllEr;
 import com.Maks.V3.Service.Services;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class SpringConfigurationFactory {
 
     @Bean
+    @Scope("prototype")
     Services servicesB(){
 
         return new Services();
 
     }
     @Bean
+    @Scope("prototype")
     ControllEr controllErB(){
 
         return  new ControllEr(servicesB());
