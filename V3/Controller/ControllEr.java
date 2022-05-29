36e0cfc5c54b5c7d12e61a3a22215b6aa3a59ccb
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-29 17:46:22 +0300

    19

--- V3/Controller/ControllEr.java
+++ V3/Controller/ControllEr.java
@@ -3,10 +3,9 @@ package com.Maks.V3.Controller;
 import com.Maks.V3.Service.RequestDTO;
 import com.Maks.V3.Service.Services;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/www")
 public class ControllEr {
 
 
@@ -23,6 +22,14 @@ public class ControllEr {
     public ResponseDTO addData(@RequestBody RequestDTO requestDTO){
 
         return services.create(requestDTO);
+
+    }
+
+    @GetMapping(value = "/f",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO findData(@RequestParam Integer id){
+
+        return services.read(id);
 
     }
 }
