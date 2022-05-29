6fa49d5964006552968ac1ef06e76466588b7dd6
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-29 17:18:04 +0300

    11

--- V3/Service/Services.java
+++ V3/Service/Services.java
@@ -1,4 +1,49 @@
 package com.Maks.V3.Service;
 
+import com.Maks.V3.Controller.ResponseDTO;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Services {
-}
+
+    List<ResponseDTO> datalist = new ArrayList<>();
+
+    public String print(String msg){
+
+        return msg;
+
+    }
+
+    public void log(String filename,ResponseDTO responseDTO){
+
+        byte[] data = ("\nName: " + responseDTO.getName() + "\nId: " + responseDTO.getId()).getBytes();
+
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+
+        try(FileOutputStream f = new FileOutputStream(filename+".txt",true);
+
+            DataOutputStream d = new DataOutputStream(new FileOutputStream(filename+".dat",true))
+        ){
+
+            b.write(data);
+
+            b.writeTo(f);
+
+            d.writeUTF("\nName: " + responseDTO.getName() + "\nId: " + responseDTO.getId());
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+
+}//EOF
