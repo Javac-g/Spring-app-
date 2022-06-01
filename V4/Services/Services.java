b7e570aa2f227e79489db2c9314d292091d5198c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-01 15:31:21 +0300

    7

--- V4/Services/Services.java
+++ V4/Services/Services.java
@@ -2,6 +2,7 @@ package com.Maks.V4.Services;
 
 import com.Maks.V4.Controller.ResponseDTO;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +12,28 @@ public class Services {
     public String print(String msg){
 
         return msg;
+
+    }
+    public void log(String filename, ResponseDTO user){
+
+        byte[] data = ("\nName: " + user.getName()+"\nId: " + user.getId()).getBytes();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+
+        try(FileOutputStream fileOutputStream = new FileOutputStream(filename+".txt",true);
+            DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(filename+".txt",true))) {
+
+            byteArrayOutputStream.write(data);
+
+            dataOutputStream.writeUTF("\nName: " + user.getName()+"\nId: " + user.getId());
+
+            byteArrayOutputStream.writeTo(fileOutputStream);
+
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+
+        }
 
     }
 
