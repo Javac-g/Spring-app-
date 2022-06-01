43afe3f5dda11eb910c48cb7e7c57e9e92851372
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-01 15:02:44 +0300

    1

--- V4/Controller/Tool.java
+++ V4/Controller/Tool.java
@@ -1,8 +1,23 @@
 package com.Maks.V4.Controller;
 
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScans
 public class Tool {
+    String first;
+    String second;
 
+    public String getFirst() {
+
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
 }
