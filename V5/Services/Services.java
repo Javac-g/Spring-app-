676e2b2f0948ce1c961b0f948dbf45a2e92b7d38
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-02 22:36:28 +0300

    14

--- V5/Services/Services.java
+++ V5/Services/Services.java
@@ -92,4 +92,24 @@ public class Services {
         }
         return null;
     }
+    public Integer delete(Integer id){
+
+        int R = -1;
+
+        for (int i = 0; i < datalist.size();i++){
+
+            if (datalist.get(i).getId().equals(id)){
+
+                R = i;
+
+                log("D",datalist.get(i));
+
+            }
+        }
+        if (R != -1){
+            datalist.remove(R);
+            return R;
+        }
+        return null;
+    }
 }
