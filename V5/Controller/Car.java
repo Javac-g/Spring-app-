77dc2a4129b0ec17b104f69393235c127921b197
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-02 20:22:03 +0300

    3

--- V5/Controller/Car.java
+++ V5/Controller/Car.java
@@ -1,4 +1,14 @@
 package com.Maks.V5.Controller;
 
 public class Car {
+
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
