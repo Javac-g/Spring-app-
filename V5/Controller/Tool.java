b3c9b3af594c36563ff4afcc586d2db0b156f128
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-02 20:29:47 +0300

    4

--- V5/Controller/Tool.java
+++ V5/Controller/Tool.java
@@ -1,4 +1,22 @@
 package com.Maks.V5.Controller;
 
 public class Tool {
+    String first;
+    String second;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
 }
