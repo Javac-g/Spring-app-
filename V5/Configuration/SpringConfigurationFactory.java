a81063996981473b68854176006e8e50d0359136
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-02 22:43:37 +0300

    15

--- V5/Configuration/SpringConfigurationFactory.java
+++ V5/Configuration/SpringConfigurationFactory.java
@@ -1,4 +1,26 @@
 package com.Maks.V5.Configuration;
 
+import com.Maks.V5.Controller.ContRolleR;
+import com.Maks.V5.Services.Services;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
 public class SpringConfigurationFactory {
+
+    @Bean(name = "Service")
+    @Scope(value = "prototype")
+    public Services servicesBean(){
+
+        return new Services();
+
+    }
+    @Bean(name = "Controller")
+    @Scope(value = "prototype")
+    public ContRolleR controllerBean(){
+
+        return new ContRolleR(servicesBean());
+
+    }
 }
--- V5/Controller/ContRolleR.java
+++ V5/Controller/ContRolleR.java
@@ -1,4 +1,13 @@
 package com.Maks.V5.Controller;
 
+import com.Maks.V5.Services.Services;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/rest")
 public class ContRolleR {
+    private Services services;
+
+    public ContRolleR(Services services) {
+        this.services = services;
+    }
 }
